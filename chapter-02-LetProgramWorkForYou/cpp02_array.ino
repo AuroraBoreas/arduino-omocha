9cfd9a45fed0c5f48309ee811d2fd02ccb1f3917
AuroraBoreas <AuroraBoreas@users.noreply.github.com>
2020-09-07 22:36:38 +0800

    update

---
+++ chapter-02-LetProgramWorkForYou/cpp02_array.ino
@@ -0,0 +1,29 @@
+int inputPins[] = {2, 3, 4, 5};
+int ledPins[] = {10, 11, 12, 13};
+
+void setup()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        /* code */
+        pinMode(inputPins[i], INPUT);
+        pinMode(ledPins[i], OUTPUT);
+        digitalWrite(inputPins[i], true);
+    }
+    
+}
+
+void loop()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        int val = digitalRead(inputPins[i]);
+        if (val == LOW)
+        {
+            digitalWrite(ledPins[i], true);
+        } else
+        {
+            digitalWrite(ledPins[i], false);
+        }
+    }
+}
